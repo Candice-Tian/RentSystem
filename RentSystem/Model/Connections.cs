7f9e407bbfbb4e294025646a790e096b0fba3f0f
Candice-Tian <tian779713952@163.com>
2022-05-02 10:58:30 +0800

    添加项目文件。

---
+++ RentSystem/Model/Connections.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentSystem.Model
+{
+    public class Connections
+    {
+        public string ConnectiongStrings { get; set; }
+
+    }
+}
